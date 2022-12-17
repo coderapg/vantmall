e985bdb11509fe51e9dc84793646c66128eb1f09
李永峰 <1184156787@qq.com>
2022-12-17 15:47:11 +0800

    引入本地字体图标库

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,15 @@
 <template>
   <div id="app">
+    <keep-alive>
+      <router-view />
+    </keep-alive>
     <van-button type="primary">主要按钮</van-button>
     <van-button type="info">信息按钮</van-button>
     <van-button type="default">默认按钮</van-button>
     <van-button type="warning">警告按钮</van-button>
     <van-button type="danger">危险按钮</van-button>
+    <van-icon class-prefix="icon" name="shoucang1" />
+    <van-icon class-prefix="icon" name="receipt-address" />
   </div>
 </template>
 
@@ -15,4 +20,6 @@ export default {
 }
 </script>
 
-<style lang="less"></style>
+<style lang="less">
+  @import url('./assets/fonts/iconfont.css');
+</style>
--- src/plugins/vant.js
+++ src/plugins/vant.js
@@ -1,8 +1,9 @@
 import Vue from 'vue'
 
 // 按需全局引入 vant组件
-import { Button, List, Cell, Tabbar, TabbarItem } from 'vant'
+import { Button, List, Cell, Tabbar, TabbarItem, Icon } from 'vant'
 Vue.use(Button)
 Vue.use(Cell)
 Vue.use(List)
 Vue.use(Tabbar).use(TabbarItem)
+Vue.use(Icon)

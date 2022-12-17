be5415b16347f48a9e6fac3a9db2aa44ec2f1ab8
李永峰 <1184156787@qq.com>
2022-12-17 21:39:26 +0800

    封装betterScroll模块

---
+++ src/components/common/BetterScroll/BetterScroll.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="wrapper" ref="wrapper">
+    <div class="content">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+import BetterScroll from 'better-scroll'
+
+export default {
+  name: 'BetterScroll',
+  data () {
+    return {
+      BScroll: null
+    }
+  },
+  mounted () {
+    this.BScroll = new BetterScroll(this.$refs.wrapper)
+    console.log('mounted===', this.BScroll)
+  }
+}
+</script>
+
+<style lang="less" scoped>
+  .wrapper {
+    overflow: hidden;
+  }
+</style>

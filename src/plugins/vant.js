8c7a92ea5387168c0f85a589fca7f9cc53057fef
李永峰 <1184156787@qq.com>
2022-12-17 15:18:02 +0800

    配置全局按需引入vant组件库

---
+++ src/plugins/vant.js
@@ -0,0 +1,8 @@
+import Vue from 'vue'
+
+// 按需全局引入 vant组件
+import { Button, List, Cell, Tabbar, TabbarItem } from 'vant'
+Vue.use(Button)
+Vue.use(Cell)
+Vue.use(List)
+Vue.use(Tabbar).use(TabbarItem)

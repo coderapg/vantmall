f402df162717d109f4a3a7bea9c3c9c0dd4291dd
李永峰 <1184156787@qq.com>
2022-12-27 18:34:09 +0800

    购物车页面完成

--- src/plugins/vant.js
+++ src/plugins/vant.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 
 // 按需全局引入 vant组件
-import { Button, List, Cell, Tabbar, TabbarItem, Icon, NavBar, Search, Toast, Swipe, SwipeItem, Image as VanImage, Grid, GridItem, Tab, Tabs, Tag, PullRefresh, Badge, Card, Rate, Progress, SubmitBar, Checkbox, CheckboxGroup, SwipeCell } from 'vant'
+import { Button, List, Cell, Tabbar, TabbarItem, Icon, NavBar, Search, Toast, Swipe, SwipeItem, Image as VanImage, Grid, GridItem, Tab, Tabs, Tag, PullRefresh, Badge, Card, Rate, Progress, SubmitBar, Checkbox, CheckboxGroup, SwipeCell, Field } from 'vant'
 Vue.use(Button)
 Vue.use(Cell)
 Vue.use(List)
@@ -23,3 +23,4 @@ Vue.use(Progress)
 Vue.use(SubmitBar)
 Vue.use(Checkbox).use(CheckboxGroup)
 Vue.use(SwipeCell)
+Vue.use(Field)
--- src/views/cart/components/CartList.vue
+++ src/views/cart/components/CartList.vue
@@ -1,21 +1,32 @@
 <template>
-  <van-list
-    v-model="loading"
-    :finished="finished"
-    finished-text="没有更多了"
-    @load="onLoad">
+  <van-list finished-text="请添加商品">
     <van-swipe-cell>
       <div class="card-content">
         <van-checkbox v-model="checked" />
-        <div>123</div>
-        <!-- <van-card
+        <van-card
           num="2"
           price="2.00"
           desc="描述信息"
           title="商品标题"
           class="goods-card"
           thumb="https://img01.yzcdn.cn/vant/cat.jpeg">
-        </van-card> -->
+        </van-card>
+      </div>
+      <template #right>
+        <van-button square text="删除" type="danger" class="delete-button" />
+      </template>
+    </van-swipe-cell>
+    <van-swipe-cell>
+      <div class="card-content">
+        <van-checkbox v-model="checked" />
+        <van-card
+          num="2"
+          price="2.00"
+          desc="描述信息"
+          title="商品标题"
+          class="goods-card"
+          thumb="https://img01.yzcdn.cn/vant/cat.jpeg">
+        </van-card>
       </div>
       <template #right>
         <van-button square text="删除" type="danger" class="delete-button" />
@@ -30,19 +41,7 @@ export default {
   name: 'CartList',
   data () {
     return {
-      loading: false,
-      finished: false,
       checked: true
-    }
-  },
-  methods: {
-    onLoad () {
-      // 异步更新数据
-      // setTimeout 仅做示例，真实场景中一般为 ajax 请求
-      setTimeout(() => {
-        // 加载状态结束
-        this.loading = false
-      }, 1000)
     }
   }
 }
@@ -52,11 +51,54 @@ export default {
   .van-list {
     margin: 10px;
     .van-swipe-cell {
-      border-radius: 15px;
+      border-radius: 10px;
+      margin-bottom: 8px;
       background-color: #fff;
       /deep/ .van-swipe-cell__wrapper {
         .card-content {
           display: flex;
+          .van-checkbox {
+            margin-left: 8px;
+            padding-right: 4px;
+          }
+          .van-card {
+            width: 100%;
+            padding: 8px 8px;
+            box-sizing: border-box;
+            .van-card__content {
+              .van-card__title {
+                color: #000;
+                font-size: 16px;
+                font-weight: 500;
+              }
+              .van-card__desc {
+                color: #999;
+                font-size: 12px;
+                margin-top: 6px;
+              }
+              .van-card__bottom {
+                display: flex;
+                justify-content: space-between;
+                .van-card__price {
+                  color: #ff6700;
+                  .van-card__price-currency {
+                    margin-right: 4px;
+                  }
+                  .van-card__price-currency,
+                  .van-card__price-decimal {
+                    font-size: 12px;
+                  }
+                  .van-card__price-integer {
+                    font-size: 16px;
+                  }
+                }
+                .van-card__num {
+                  font-size: 16px;
+                  color: #000;
+                }
+              }
+            }
+          }
         }
       }
     }
